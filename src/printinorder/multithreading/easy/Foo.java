19b5ed8d6c05e02f94bef72afed5489369a81499
JSAR <jnsartwell@gmail.com>
2020-03-20 14:20:00 -0400

    Pushing a possible easy solution

--- src/printinorder/multithreading/easy/Foo.java
+++ src/printinorder/multithreading/easy/Foo.java
@@ -2,18 +2,35 @@ package printinorder.multithreading.easy;
 
 class Foo {
 
-    public  Foo() {
+    private boolean firstLocked;
+    private boolean secondLocked;
+    private boolean thirdLocked;
+
+    Foo() {
+        firstLocked = false;
+        secondLocked = true;
+        thirdLocked = true;
     }
 
     void first(Runnable printFirst) {
+        while (firstLocked) {
+        }
         printFirst.run();
+        firstLocked = true;
+        secondLocked = false;
     }
 
     void second(Runnable printSecond) {
+        while (secondLocked) {
+        }
         printSecond.run();
+        secondLocked = true;
+        thirdLocked = false;
     }
 
     void third(Runnable printThird) {
+        while (thirdLocked) {
+        }
         printThird.run();
     }
 
